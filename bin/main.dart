839e5aa2b9453dfc562c7b39b428a798cce24c98
Ashish <ashishteddy2@gmail.com>
2019-11-21 16:21:20 +0530

    Done 2.0

--- bin/main.dart
+++ bin/main.dart
@@ -1,34 +1,56 @@
 // Challenge
 // Simplified Fractions
 // Create a function that returns the simplified version of a fraction.
-import 'package:starfruit/starfruit.dart';
-
 /// Examples
 //  simplify("4/6") ➞ "2/3"
 
-String simplify(String input){
-  List numbers = input.split('/').toList();
-  int num1 = int.parse(numbers[0]);
-  int num2 = int.parse(numbers[1]);
-  int i = 1;
-  while((num1 % num2 == 0 || num2 % num1 == 0) && !mUtils.isPrime(num1) || !mUtils.isPrime(num2)){
+import 'dart:math';
+//String simplify1(String input){
+//  List numbers = input.split('/').toList();
+//  int num1 = int.parse(numbers[0]);
+//  int num2 = int.parse(numbers[1]);
+//  int i = 1;
+//  while((num1 % num2 == 0 || num2 % num1 == 0) && !isPrime(num1) || !isPrime(num2)){
+//
+//    if (num1 % i == 0 && num2 % i == 0){
+//      num1 = (num1 / i).round();
+//      num2 = (num2 / i).round();
+//    }
+//    i++;
+//    if (i == 100){
+//      break;
+//    }
+//  }
+//  if (num1 == num2){
+//    return '1';
+//  }
+//  else{
+//    return '$num1/$num2';
+//  }
+//}
 
-    if (num1 % i == 0 && num2 % i == 0){
-      num1 = (num1 / i).round();
-      num2 = (num2 / i).round();
-    }
-    i++;
-    if (i == 1000){
-      break;
-    }
-  }
-  if (num1 == num2){
-    return '1';
-  }
-  else{
-    return '$num1/$num2';
-  }
+String simplify(String number){
+  List numbers = (number.split('/'));
+  int num = int.parse(numbers[0]);
+  int den = int.parse(numbers[1]);
+  int factor = calculateHcf(num, den);
+  num = num~/factor;
+  den = den~/factor;
+  return('$num/$den');
 }
+
+int calculateHcf(int x, int y){
+  int smaller = min(x, y);
+  int hcf;
+  for (int i = 1; i< smaller+1; i++){
+    if(x%i == 0 && y%i == 0){
+      hcf = i;
+    }
+  }
+  return hcf;
+}
+
+
 main() {
-  print(simplify("1000/52"));
+  print(simplify("8646544864/8496874112"));
 }
--- bin/test.dart
+++ bin/test.dart
@@ -4,7 +4,10 @@ import 'package:test/test.dart';
 
     void main() {
       test("test case 1", () {
-        expect(simplify('6/4'), '3/2');
-        expect(simplify('2/2'), '1');
+        expect(simplify('250/300'), '5/6');
+        expect(simplify('2/2'), '1/1');
+        expect(simplify("88/44"), ("2/1"));
+        expect(simplify("477/381"), ("159/127"));
+        expect(simplify("1026/4478"), ("513/2239"));
       });
     }
